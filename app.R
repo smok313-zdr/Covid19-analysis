9460ffa9210fb66557660e909df1fda592ef082d
smok313-zdr <traxer@op.pl>
2023-04-20 23:40:24 +0200

    Added leaflet map

--- app.R
+++ app.R
@@ -2,12 +2,22 @@ library(shiny)
 library(shinydashboard)
 library(rCharts)
 library(dygraphs)
+library(leaflet)
 library(dplyr)
 
 # data
-country_wise_latest <- read.csv("country_wise_latest.csv")
 day_wise <- read.csv("day_wise.csv")
+countries_long_lat <- read.csv("average-latitude-longitude-countries.csv")
 worldometer_data <- read.csv("worldometer_data.csv")
+colnames(countries_long_lat)[2] <- "Country.Region"
+map_coordinates <- merge(countries_long_lat, worldometer_data, by = "Country.Region")
+missing_coordinates <- data.frame(Country.Region=worldometer_data$Country.Region[!worldometer_data$Country.Region %in% map_coordinates$Country.Region],
+                                  Latitude=c(38,60,32,51.5,24,47,49.45,7.32,37,31.56,41.50,-4.02,25,6.37,-26.30,16,7.51,35,NA,-21.07,49.37,-6.22,54,61.53,18.02,4.30,21.8,13.15,18.05,12.12,-8.55,18,12.20,-51.56,41.90),
+                                  Longitude=c(-97,100,54,-0.13,54,29,15.30,-5.54,127.30,35.13,22,21.45,17,20.56,31.30,-24,29.41,38,NA,55.31,-2.36,34.53,-4.46,-6.58,-63.04,114.40,-72.3,-61.11,-63.03,-68.56,125.56,105,-68.26,-59.82,12.45),
+                                  TotalCases=c(5032179,871894,320117,308134,61845,26628,17731,16447,14519,13398,11399,9309,4879,4620,2968,2734,2450,999,NA,671,597,509,336,266,160,141,129,56,53,31,25,20,13,13,12))
+missing_coordinates <- missing_coordinates %>% na.omit()
+map_coordinates <- map_coordinates %>% select(Country.Region,Latitude,Longitude,TotalCases)
+map_coordinates <- rbind(map_coordinates,missing_coordinates)
 countries <- unique(worldometer_data$Country.Region)
 continents <- unique(worldometer_data$Continent)
 
@@ -17,7 +27,6 @@ header <- dashboardHeader(title = " ")
 
 side <- dashboardSidebar(selectInput('country', 'Country/region', countries),
                          selectInput('continent', 'Continent', continents, selected = "Europe"),br(),p(),
-                         # add slider for months
                          br(),p(),
                          div(img(src="covid19disease.jpg")),br(),p("Author:"),
                          a(href= "https://stackoverflow.com/users/12382064/kaczdr",icon("fa fa-stack-overflow", "fa-2x")),
@@ -39,14 +48,15 @@ body <- dashboardBody(
     ),
     fixedRow(
         column(width = 4,box(title = "Total Cases by Country", status = "warning",solidHeader = TRUE,showOutput("Chart3", "nvd3"),htmlOutput("other"),width = 12)),
-        column(width = 4,box(title = "Country-specific pandemic statistics", status = "warning",solidHeader = TRUE,showOutput("Chart4", "HighCharts"),width = 12))
+        column(width = 4,box(title = "Country-specific pandemic statistics", status = "warning",solidHeader = TRUE,showOutput("Chart4", "HighCharts"),width = 12)),
+        column(width = 4,box(title = "Map of Covid-19 Total Cases", status = "danger",solidHeader = TRUE,leafletOutput("map"),width = 12))
     )
 )
 
 ui <- dashboardPage(header, side, body) 
 
 # server.r
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     continentData <- reactive({
         selectedContinent <- worldometer_data[worldometer_data$Continent == input$continent, ]
@@ -69,8 +79,9 @@ server <- function(input, output) {
     output$Chart2 <- renderChart2({ 
         b <- worldometer_data %>% filter(Continent != "") %>% group_by(Continent) %>% summarise(TotalCases = sum(TotalCases))
         n1 <- mPlot(TotalCases ~ Continent, data = b, type = "Bar")
+        n1$set(hideHover = "auto") 
         #n1$set(width = 900)
-        n1$params$width <- 900
+        #n1$params$width <- 900
         return(n1)
     })
     
@@ -109,12 +120,21 @@ server <- function(input, output) {
         dat <- data.frame(key = colnames(countryData())[c(6,8,10)], value = c(countryData()$TotalDeaths,countryData()$TotalRecovered,countryData()$ActiveCases))
         h1 <- hPlot(x = "key", y = "value", data = dat, type = "pie", title = countryData()$Country.Region)
         h1$chart(
-            width = 500
+            width = 450
         )
         return(h1)
     })
     
-    # interaktywny summary z danych
+    output$map <- renderLeaflet({
+        leaflet(map_coordinates) %>% addTiles() %>%
+            fitBounds(~min(Longitude), ~min(Latitude), ~max(Longitude), ~max(Latitude)) %>% addCircleMarkers(
+                data = map_coordinates,
+                lat = map_coordinates$Latitude,
+                lng = map_coordinates$Longitude,
+                radius = ~map_coordinates$TotalCases/100000, popup = paste0(map_coordinates$Country.Region)
+            )
+    })
+    
 }
 
 shinyApp(ui = ui, server = server)
